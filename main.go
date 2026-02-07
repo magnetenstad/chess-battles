05e0edc8a62c4ea7fd33b2f8701a3b8367c6234a
Gregor Moe <moethegregor@gmail.com>
2026-02-07 12:42:04 +0100

    Define Game, Board, Player, Cell, and Piece structures for game logic

--- main.go
+++ main.go
@@ -18,7 +18,41 @@ func init() {
 	}
 }
 
-type Game struct{}
+type Game struct {
+	Board1      *Board
+	Board2      *Board
+	Player1     *Player
+	Player2     *Player
+	IsGameOver  bool
+	Winner      int
+	TickCounter uint64
+}
+
+type Board struct {
+	Width, Height int
+	Cells         [][]Cell
+}
+
+type Player struct {
+	Name   string
+	Id     string
+	Health int
+	Cash   int
+}
+
+type Cell struct {
+	X, Y     int
+	occupant Piece
+}
+
+type Piece int
+
+const (
+	PawnUnit Piece = iota
+	KnightUnit
+	TowerUnit
+	BishopUnit
+)
 
 func (g *Game) Update() error {
 	return nil
