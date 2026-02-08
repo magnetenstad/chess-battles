aa1e8926d4ec9a11774b0d404a0a9a6f0349a55f
magne.tenstad <tenstad.magne@gmail.com>
2026-02-08 17:55:32 +0100

    Return early

--- src/state_play.go
+++ src/state_play.go
@@ -4,20 +4,20 @@ import "time"
 
 func (g *Game) UpdateStatePlay() {
 	now := time.Now()
-	if now.Sub(g.PrevComputerTime).Seconds() >= (1 / ComputerFPS) {
-		g.PrevComputerTime = now
+	if now.Sub(g.PrevComputerTime).Seconds() < (1 / ComputerFPS) {
+		return
+	}
 
-		board := &g.Board
-		move, ok := ComputeMove(board, 6)
+	g.PrevComputerTime = now
+	board := &g.Board
+	move, ok := ComputeMove(board, 6)
 
-		if ok {
-			target := board.Tiles[move.To.Y][move.To.X]
+	if ok {
+		target := board.Tiles[move.To.Y][move.To.X]
+		ApplyMove(board, move)
 
-			ApplyMove(board, move)
-
-			if target.King {
-				g.State = StateArrange
-			}
+		if target.King {
+			g.State = StateArrange
 		}
 	}
 }
