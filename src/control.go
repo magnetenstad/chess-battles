85a1a87ec1ca3e679cd961a6bfd634412d374c29
magne.tenstad <tenstad.magne@gmail.com>
2026-02-08 20:26:02 +0100

    Fix bug when first move wins game

--- src/control.go
+++ src/control.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
 
 func (graphics *Graphics) DrawControl(screen *ebiten.Image) {
 	opt := graphics.Position(200, 200)
@@ -10,7 +13,7 @@ func (graphics *Graphics) DrawControl(screen *ebiten.Image) {
 }
 
 func (game *Game) UpdateControl() {
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
 		x, y := ebiten.CursorPosition()
 		if x > 200 && x < 200+TileSize*3 && y > 200 && y < 200+TileSize {
 			game.State = StatePlay
